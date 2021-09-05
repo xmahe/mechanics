dbe7643e86c8fdc14e5dba2ce01ddf56cd285567
Magnus Hedlund <magnus@hedlundinnovation.se>
2021-09-05 09:56:26 +0200

    Stuff

--- stick.py
+++ stick.py
@@ -18,22 +18,45 @@ class Vector:
         return Vector(self.x*factor, self.y*factor)
 
 class Node:
+    FIXED = 0
     def __init__(self, p, v, m):
         self.p = p
         self.v = v
         self.m = m
         self.f = Vector(0, 0)
+        self.condition = None
+
+class Link:
+    def __init__(self, node_a, node_b):
+        self.a = node_a
+        self.b = node_b
+    def apply_link_forces(self):
+        # This is a rigid link
+        # That means that forces in the link direction must be propagated through the link
+        # pa = (1,1), ma = 1
+        # pb = (2,2), mb = 2
+        # external forces on nodes are fa = (0,1)
+        # maybe b is rigid. what forces should b exert via the link to not move?
+        # the direction of forces on b from a is (pa-pb)/|pa-pb| = (-1, -1)/2 = (-0.5, -0.5)
+
+        pass
+
 
 nodes = []
 
 
-nodes.append(Node(Vector(100,100), Vector(0.1,0), 1))
+nodes.append(Node(Vector(100,100), Vector(0, 0), 1))
+nodes.append(Node(Vector(100,100), Vector(0.1, 0), 1))
+nodes.append(Node(Vector(230,100), Vector(33, 100), 1))
+
+
+nodes[0].condition = Node.FIXED
+
 
 while running:
     # Increase time
-    dt = clock.tick(50)
-    t += dt*1e-3
-    screen.fill((0,0,0))
+    dt = clock.tick(50)*1e-3
+    t += dt
 
     # Handle events
     for event in pygame.event.get():
@@ -52,13 +75,10 @@ while running:
         #    pass
 
     # Compute physics
-    #x = 50*sin(2*pi*t)
-
     ## Compute forces
     for node in nodes:
         # Gravity
-        node.f += Vector(0, 0.001).scale(node.m)
-
+        node.f += Vector(0, 10/dt).scale(node.m)
         # Floor
         if node.p.y > 280:
             node.p.y = 280
@@ -68,16 +88,33 @@ while running:
                 node.v.y *= -0.4
             node.v.x *= 0.95
 
-    ## Compute positions
     for node in nodes:
-        node.v += node.f.scale(1/node.m*dt)
-        node.p += node.v.scale(dt)
+        if node.condition == Node.FIXED:
+            # Make sure this stays still by zeroing out all forces acting upon it from neighbours
+            node.f = Vector(0, 0)
+
+    ## Compute step
+    for node in nodes:
+        def RK45(dt, f, t, x):  # Runge-Kutta 45, solves f(t,x) = dx/dt
+            k1 = dt*f(t, x)
+            k2 = dt*f(t+dt/2, x+k1/2)
+            k3 = dt*f(t+dt/2, x+k2/2)
+            k4 = dt*f(t+dt  , x+k3  )
+            return x + k1/6 + k2/3 + k3/3 + k4/6
+
+        # acc = force / mass = dvelocity/dt, f(t,velocity) = force/mass (a constant)
+        node.v.x = RK45(dt, lambda t, f: node.f.x/node.m, t, node.v.x)
+        node.v.y = RK45(dt, lambda t, f: node.f.y/node.m, t, node.v.y)
+        # dposition/dt = velocity = f(t,position) (a constant as well)
+        node.p.x = RK45(dt, lambda t, f: node.v.x       , t, node.p.x)
+        node.p.y = RK45(dt, lambda t, f: node.v.y       , t, node.p.y)
 
     ## Reset forces
     for node in nodes:
         node.f = Vector(0, 0)
 
     # Draw graphics
+    screen.fill((0,0,0))
     ## Nodes
     for node in nodes:
         pygame.draw.circle(screen, (200,200,255), (node.p.x, node.p.y), 5)
